1cf7dabc6ca958847da1f206baba3c382351b5a7
Hikmat <xikmatkhiva.4s@gmail.com>
2026-02-20 14:46:34 +0500

    Update socket.io.js

--- backend/socket.io.js
+++ backend/socket.io.js
@@ -1,17 +1,17 @@
-import { Server } from "socket.io";
-export function initSocket(server) {
-  const CORS_ORIGIN = process.env?.CORS_ORIGIN || "*";
-  const io = new Server(server, {
-    cors: {
-      //   origin: CORS_ORIGIN,
-      origin: true,
-      credentials: true,
-      methods: ["GET","POST"],
-    },
-  });
-  io.on("connection", (socket) => {
-    console.log("Client connected:", socket.id);
-  });
+// import { Server } from "socket.io";
+// export function initSocket(server) {
+//   const CORS_ORIGIN = process.env?.CORS_ORIGIN || "*";
+//   const io = new Server(server, {
+//     cors: {
+//       //   origin: CORS_ORIGIN,
+//       origin: true,
+//       credentials: true,
+//       methods: ["GET","POST"],
+//     },
+//   });
+//   io.on("connection", (socket) => {
+//     console.log("Client connected:", socket.id);
+//   });
 
-  return io;
-}
+//   return io;
+// }
